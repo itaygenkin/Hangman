00e2f08777aa09405d2e4e0f77c6f2943c010bb6
Itay Genkin <itay.genkin@gmail.com>
2022-02-12 18:17:27 +0200

    12/02 - adding some delays betweent printing

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import string
+import sys
+import time
 
 HANGMAN_ASCII_ART = """  _    _
  | |  | |
@@ -54,13 +57,16 @@ old_letters_guessed = []
 # printing the begging screen of the game
 def welcome():
     print(HANGMAN_ASCII_ART)
+    time.sleep(1.3)
     print("Welcome to the game Hangman")
-    print("You have 6 tries")  #
+    time.sleep(1.8)
+    print("You have 6 tries")
+    time.sleep(1.7)
     enter = input("Please press enter to begin the game!")
 
 
 def main():
-    words_file = open("D:\Hangman words.txt", 'r')
+    words_file = open(sys.argv[1], 'r')
     word_index = int(input("Please enter a number "))
     secret_word = choose_word(words_file, word_index)
 
@@ -74,17 +80,19 @@ def main():
 
         if try_update_letter_guessed(letter_guessed, old_letters_guessed):
             if letter_guessed not in secret_word:
-                print("wrong guess :(\nplease try again")
+                print("Wrong guess :(")
+                time.sleep(0.5)
                 num_of_tries += 1
                 print_hangman(num_of_tries)
+                time.sleep(0.6)
+                print("Please try again")
             elif check_win(secret_word, old_letters_guessed):
                 print(show_hidden_word(secret_word, old_letters_guessed))
                 print("Congratulations!")
                 break
-            #else:
-               # print(show_hidden_word(secret_word, old_letters_guessed))
         else:
-            print("invalid letter, please try again")
+            time.sleep(0.3)
+            print("Invalid letter, please try again")
         if num_of_tries == MAX_TRIES:
             print("Loser!!")
         words_file.close()
@@ -102,19 +110,18 @@ def choose_word(file_path, index):
 
 # return True iff the letter is a letter from the 'abc' (no matter if the letter is upper or lower)
 def is_valid_letter(letter):
-    abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    letter.lower()
+    abc = string.ascii_lowercase
     if len(letter) == 1 and letter in abc:
         return True
-    else:
-        return False
+    return False
 
 
 # return True iff the input of the player is valid
 def check_valid_input(letter_guessed, old_letters_guessed):
     if not is_valid_letter(letter_guessed) or letter_guessed in old_letters_guessed:
         return False
-    else:
-        return True
+    return True
 
 
 # update the letters the user guessed and return true iff the letter guessed is valid and it is not guessed yet
@@ -124,9 +131,10 @@ def try_update_letter_guessed(letter_guessed, old_letters_guessed):
         return True
     else:
         print("X")
+        time.sleep(0.3)
         separator = ' --> '
         old_letters_guessed.sort()
-        print(separator.join(old_letters_guessed))
+        print("Letters guessed: " + separator.join(old_letters_guessed))
         return False
 
 
@@ -138,55 +146,55 @@ def show_hidden_word(secret_word, old_letters_guessed):
             word_shown = word_shown + letter + ' '
         elif letter == '_':
             word_shown = word_shown + '- '
-        else :
+        else:
             word_shown = word_shown + "_ "
     return word_shown[:-1]
 
 
 # return True iff the player has won
 def check_win(secret_word, old_letters_guessed):
-    for letter in secret_word :
+    for letter in secret_word:
         if letter not in old_letters_guessed and letter != '_':
             return False
     return True
 
 
 # the names of the following keys were changed to just numbers so as to simplify the next function
-HANGMAN_PHOTOS = {0 : "  x-------x",
-1 : """    x-------x
+HANGMAN_PHOTOS = {0: "  x-------x",
+                  1: """    x-------x
     |
     |
     |
     |
     |""",
-2 : """"    x-------x
+                  2: """"    x-------x
     |       |
     |       0
     |
     |
     |
 """,
-3 : """    x-------x
+                  3: """    x-------x
     |       |
     |       0
     |       |
     |
     |
 """,
-4 : """    x-------x
+                  4: """    x-------x
     |       |
     |       0
     |      /|\\
     |
     |
 """,
-5 : """    x-------x
+                  5: """    x-------x
     |       |
     |       0
     |      /|\\
     |      /
     |""",
-6 : """    x-------x
+                  6: """    x-------x
     |       |
     |       0
     |      /|\\
@@ -197,5 +205,7 @@ HANGMAN_PHOTOS = {0 : "  x-------x",
 def print_hangman(num_of_tries):
     print(HANGMAN_PHOTOS[num_of_tries])
 
-welcome()
-main()
+
+if __name__ == '__main__':
+    welcome()
+    main()
